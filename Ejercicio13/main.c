c7ff52a4094af81833e8a5884c0898741e4f9c37
Facundo <facaagus@gmail.com>
2018-08-27 19:50:10 -0300

    Ejercicios

---
+++ Ejercicio13/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+
+
+    for ( numero = 100 ; numero >= 0 ; numero -- )
+    {
+        printf( "\n   " );
+        printf( "%d ", numero );
+    }
+
+
+    return 0;
+}
---
+++ Ejercicio14/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int numero;
+
+
+    for ( numero = 3 ; numero < 100; numero += 3 )
+    {
+        printf( "\n   " );
+        printf( "%d ", numero );
+    }
+
+
+    return 0;
+}
